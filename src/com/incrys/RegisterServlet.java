48ce14cde0c6372d5d973b1d5035811e104e702f
voinicu <voinicu@InCrys193.com>
2017-07-11 15:53:14 +0300

    Full validation of fields inside User class

--- src/com/incrys/RegisterServlet.java
+++ src/com/incrys/RegisterServlet.java
@@ -6,11 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 public class RegisterServlet extends HttpServlet {
     @Override
@@ -22,12 +19,12 @@ public class RegisterServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String username = req.getParameter("username");
         String email = req.getParameter("email");
-        DateFormat format = new SimpleDateFormat("yyyy-mm-dd");
-        Date birthDate = null;
-        try {
-            birthDate = format.parse(req.getParameter("birthDate"));
-        } catch (ParseException e) {
-            e.printStackTrace();
+        LocalDate birthDate;
+        try{
+            birthDate = LocalDate.parse(req.getParameter("birthDate"));
+
+        }catch (DateTimeParseException e) {
+            birthDate=LocalDate.now();
         }
         String password = req.getParameter("pwd");
         String confirmedPassword = req.getParameter("pwd2");
@@ -37,5 +34,11 @@ public class RegisterServlet extends HttpServlet {
 
         PrintWriter printWriter = resp.getWriter();
         printWriter.append(user.toString());
+        if(user.validateUser()) {
+            printWriter.append("SUCCESS");
+        }
+        else{
+            printWriter.append("\n\n"+user.message);
+        }
     }
 }
--- src/com/incrys/User.java
+++ src/com/incrys/User.java
@@ -1,22 +1,28 @@
 package com.incrys;
 
+import java.time.LocalDate;
 import java.util.Date;
+import java.util.StringJoiner;
 
 public class User {
     private String username;
-    private Date dateOfBirth;
+    private LocalDate dateOfBirth;
     private String email;
     private String password;
     private String confirmedPassword;
     private String secretCode;
 
-    public User(String username, Date dateOfBirth, String email, String password, String confirmedPassword, String secretCode) {
+    public StringJoiner message;
+
+    public User(String username, LocalDate dateOfBirth, String email, String password, String confirmedPassword, String secretCode) {
         this.username = username;
         this.dateOfBirth = dateOfBirth;
         this.email = email;
         this.password = password;
         this.confirmedPassword=confirmedPassword;
         this.secretCode = secretCode;
+
+        this.message=new StringJoiner("\n");
     }
 
     public String getUsername() {
@@ -27,11 +33,11 @@ public class User {
         this.username = username;
     }
 
-    public Date getDateOfBirth() {
+    public LocalDate getDateOfBirth() {
         return dateOfBirth;
     }
 
-    public void setDateOfBirth(Date dateOfBirth) {
+    public void setDateOfBirth(LocalDate dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
 
@@ -71,10 +77,70 @@ public class User {
                 '}';
     }
 
-    public boolean validateUsername(){
+    private boolean validateUsername(){
         boolean isValid = true;
-        if(this.username.length()>100) isValid=false;
-        if(this.username.length()==0) isValid=false;
+        if(this.username.length()>100) {
+            isValid = false;
+            message.add("Username too long.");
+            return isValid;
+        }
+        if(this.username.length()==0) {
+            isValid = false;
+            message.add("Username is null.");
+            return isValid;
+        }
+        return isValid;
+    }
+
+    private boolean validateBirthDate() {
+        boolean isValid = true;
+        if (dateOfBirth.isAfter(LocalDate.now().minusYears(18))) {
+            isValid = false;
+            message.add("Must be older than 18.");
+        }
+        return isValid;
+    }
+
+    private boolean validatePassword(){
+        boolean isValid=true;
+        if(!this.password.equals(this.confirmedPassword)) {
+            isValid=false;
+            message.add("Passwords do not match.");
+            return isValid;
+        }
+        if(this.password.length()<8 || this.password.length()>100) {
+            isValid = false;
+            message.add("Password length invalid.");
+            return isValid;
+        }
+        if(!this.password.matches(".*\\d+.*")){
+            isValid = false;
+            message.add("Password must contain a number.");
+            return isValid;
+        }
+        if(!this.password.matches(".*[A-Z].*")){
+            isValid = false;
+            message.add("Password must contain at least an uppercase letter.");
+            return isValid;
+        }
+        return isValid;
+    }
+
+    private boolean validateEmail(){
+        if(!this.email.matches("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$")){
+            message.add("Email invalid.");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean validateUser(){
+        boolean isValid=true;
+        if(!this.validateUsername()) isValid=false;
+        if(!this.validateBirthDate()) isValid=false;
+        if(!this.validatePassword()) isValid=false;
+        if(!this.validateEmail()) isValid=false;
         return isValid;
     }
 
